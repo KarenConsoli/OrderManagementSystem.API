84c38796240cc99beb064a72392f6333cd60cb95
Karen Consoli <kbc_95@hotmail.com>
2024-01-22 18:48:09 -0300

    API

---
+++ OrderManagementSystem.API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderManagementSystem.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductService _productService;
+
+        public ProductsController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+        // Implement actions here.
+    }
+}
